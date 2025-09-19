d7ada786e24350a39f4c9fa7dce140c440f7ef7c
Arjun M S <64315213+arjun-ms@users.noreply.github.com>
2025-09-19 08:30:10 +0530

    Update hollow_rectangle.py

--- hollow_rectangle.py
+++ hollow_rectangle.py
@@ -22,6 +22,21 @@ def print_pattern(n):
         
 print_pattern(6)
 
+#======================== EASY CODE ======================
+
+def print_pattern(n):
+    
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            # print star at borders
+            if(i==1 or i==n or j==1 or j==n):
+                print("*", end="")
+            else:
+                print(" ", end="")
+        # move to next line after each row     
+        print()
+        
+print_pattern(6)
 
 """
 
@@ -33,3 +48,5 @@ print_pattern(6)
 * * * * * *
 
 """
+
+
